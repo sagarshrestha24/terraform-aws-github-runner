61bdd4dc2337a684c6be29130efc46b9e9526738
sagarshrestha24 <76894861+sagarshrestha24@users.noreply.github.com>
2023-12-14 13:53:22 +0545

    Update variables.tf

--- modules/ami-housekeeper/variables.tf
+++ modules/ami-housekeeper/variables.tf
@@ -127,11 +127,15 @@ variable "lambda_architecture" {
 variable "tracing_config" {
   description = "Configuration for lambda tracing."
   type = object({
-    mode                  = optional(string, null)
-    capture_http_requests = optional(bool, false)
-    capture_error         = optional(bool, false)
+    mode                  = string
+    capture_http_requests = bool
+    capture_error         = bool
   })
-  default = {}
+  default = {
+    mode                  = null
+    capture_http_requests = false
+    capture_error         = false
+  }
 }
 
 # specif for this module
